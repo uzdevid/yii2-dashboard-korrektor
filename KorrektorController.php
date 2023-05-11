2621659e820ba5371698a4ce63d902b04d4aa747
uzdevid <uzdevid@gmail.com>
2023-05-11 13:58:42 +0500

    first commit

---
+++ KorrektorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace uzdevid\dashboard\korrektor;
+
+/**
+ * This is just an example.
+ */
+class KorrektorController extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
